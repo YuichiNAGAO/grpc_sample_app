05a2ece6af433f665f8f63c5d30c2a8784c3dd88
YuichiNAGAO <azkgfa03nagao@gmail.com>
2023-07-03 09:41:23 +0900

    InMemoryLaptopStoreの実装

---
+++ service/laptop_store.go
@@ -0,0 +1,47 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+
+	"github.com/YuichiNAGAO/grpc_sample_app/pb"
+	"github.com/jinzhu/copier"
+)
+
+var ErrAlreadyExists = errors.New("record already exists")
+
+type LaptopStore interface {
+	Save(laptop *pb.Laptop) error
+}
+
+type InMemoryLaptopStore struct {
+	mutex sync.RWMutex
+	data  map[string]*pb.Laptop
+}
+
+func NewInMemoryLaptopStore() LaptopStore {
+	return &InMemoryLaptopStore{
+		data: make(map[string]*pb.Laptop),
+	}
+}
+
+func (store *InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	if store.data[laptop.Id] != nil {
+		return ErrAlreadyExists
+	}
+
+	// deep copy
+	other := &pb.Laptop{}
+	err := copier.Copy(other, laptop)
+	if err != nil {
+		return fmt.Errorf("cannot copy laptop data: %w", err)
+	}
+
+	store.data[other.Id] = other
+
+	return nil
+}
