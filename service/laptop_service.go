0acef7921ceff524bd407a0c9aaa99f70a08ca7f
YuichiNAGAO <azkgfa03nagao@gmail.com>
2023-07-03 09:34:41 +0900

    laptop serviceの実装

---
+++ service/laptop_service.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"context"
+	"log"
+
+	"github.com/YuichiNAGAO/grpc_sample_app/pb"
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type LaptopServer struct {
+}
+
+func NewLaptopServer() *LaptopServer {
+	return &LaptopServer{}
+}
+
+func (server *LaptopServer) CreateLaptop(ctx context.Context, req *pb.CreateLaptopRequest) (*pb.CreateLaptopResponse, error) {
+	laptop := req.GetLaptop()
+	log.Printf("receive a create-laptop request with id: %s", laptop.Id)
+
+	if len(laptop.Id) > 0 {
+		// check if the id is valid
+		_, err := uuid.Parse(laptop.Id)
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "laptop ID is not a valid UUID: %v", err)
+		}
+	} else {
+		id, err := uuid.NewRandom()
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "cannot generate a new laptop ID: %v", err)
+		}
+		laptop.Id = id.String()
+	}
+
+	// save the laptop to in-memory storage
+
+	return nil, nil
+}
